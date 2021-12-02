fd31f2a803b17b69fc55ec1e126b262f38108590
Alexandre Kelifa <alexandre.kelifa@hotmail.com>
2021-12-02 13:33:52 +0100

    add text on background, resize

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
     return (
         <div className="home">
 
-            <span className="home__img"/>
+            <span className="home__img">La plateforme de rendez-vous du coup de main numérique</span>
 
             <p className="home__p">
                 Vous êtes sur la plateforme de prise de rendez-vous du coup de main numérique <br />
