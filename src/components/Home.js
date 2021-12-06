b231c2a959b98b7b1f872b754160018a0bbfaa96
Alexandre Kelifa <alexandre.kelifa@hotmail.com>
2021-12-06 15:24:32 +0100

    homepage adjustements

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
     return (
         <div className="home">
 
-            <span className="home__img">La plateforme de rendez-vous du coup de main numérique</span>
+            <h1 className="home__img">La plateforme de rendez-vous du coup de main numérique</h1>
 
             <p className="home__p">
                 Vous êtes sur la plateforme de prise de rendez-vous du coup de main numérique <br />
