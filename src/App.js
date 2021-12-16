143f9a3b0e1d4e73a34dc1513d9bf850162b5192
Alexandre Kelifa <alexandre.kelifa@hotmail.com>
2021-12-16 18:12:51 +0100

    add react-router and home route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo_LCDMN.jpg';
 import './App.css';
+import {Routes, Route} from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
 import Footer from './components/Footer'
@@ -7,9 +8,20 @@ import Footer from './components/Footer'
 function App() {
   return (
     <div className="App">
-      <Header className="App-header"/>
-      <Home className="App-home"/>
-      <Footer className="App-footer"/>
+
+      <Header />
+      <Routes>
+
+        <Route path="/" element={
+        <Home />} />
+          {/* <Header className="App-header"/>
+          <Home className="App-home"/>
+          <Footer className="App-footer"/> */}
+        
+      </Routes>
+
+      <Footer />
+
     </div>
   );
 }
